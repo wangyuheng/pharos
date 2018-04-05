d3dbae1e030db6130d7fa6b37883ddddced451d0
Crick <wangyuheng@outlook.com>
2018-04-05 23:41:38 +0800

    add ping check

--- src/main/java/com/crick/business/pharos/config/WebConfig.java
+++ src/main/java/com/crick/business/pharos/config/WebConfig.java
@@ -2,14 +2,17 @@ package com.crick.business.pharos.config;
 
 import com.crick.business.pharos.aop.AuthorInterceptor;
 import com.crick.business.pharos.aop.SignInterceptor;
+import com.crick.business.pharos.job.PingCheckTask;
 import com.crick.business.pharos.service.AlertTextBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableScheduling
 public class WebConfig implements WebMvcConfigurer {
 
     @Value("${wechat.cp.agentid}")
@@ -26,10 +29,10 @@ public class WebConfig implements WebMvcConfigurer {
         return new SignInterceptor();
     }
 
-//    @Bean
-//    public PingCheckTask pingCheckTask(){
-//        return new PingCheckTask();
-//    }
+    @Bean
+    public PingCheckTask pingCheckTask(){
+        return new PingCheckTask();
+    }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
